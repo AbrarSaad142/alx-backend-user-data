bc2a7205164ac43997844645ad60e1bc22e033fc
Abrar Saad <abrarsaad3572022@gmail.com>
2024-09-03 22:40:01 +0300

    3. Auth class3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Authentication module"""
+from typing import List, TypeVar
+from flask import request
+import fnmatch
+
+
+class Auth:
+    """Authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """hat returns False - path and excluded_paths"""
+        if path:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        """that returns None - request will be the Flask request object"""
+        if request:
+            return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        """that returns None - request will be the Flask request object"""
+        return None
